0a64f975483a5e505a429dae77759f10b1c128e2
Vladimir <avre18c@yandex.ru>
2022-10-21 10:21:36 +0700

    fix

--- js/main.js
+++ js/main.js
@@ -1,11 +1,10 @@
 const PHOTO_INFO_COUNT = 25;
-const GENER_OBJ_MIN = 1;
-const GENER_OBJ_MAX = 25;
 const LIKE_MIN = 15;
 const LIKE_MAX = 200;
 const COMMENTS_MIN = 0;
 const COMMENTS_MAX = 200;
-
+let idNumber = 1;
+let photoNumber = 1;
 
 const getRandomPositiveInteger = (a, b) => {
   if (a < 0 || b < 0) {
@@ -22,13 +21,11 @@ const checkLengthString = (string, maxLength) => string.length <= maxLength;
 checkLengthString(2, 5);
 
 const createPhotoInfo = () => ({
-  id: getRandomPositiveInteger(GENER_OBJ_MIN, GENER_OBJ_MAX),
-  url: `photos/${getRandomPositiveInteger(GENER_OBJ_MIN, GENER_OBJ_MAX)}.jpg`,
+  id: idNumber++,
+  url: `photos/${photoNumber++}.jpg`,
   description: 'Лучшее фото в мире',
   likes: getRandomPositiveInteger(LIKE_MIN, LIKE_MAX),
   comments: getRandomPositiveInteger(COMMENTS_MIN, COMMENTS_MAX),
 });
 
-const array = Array.from({ length: PHOTO_INFO_COUNT }, createPhotoInfo);
-
-console.log(array);
+Array.from({ length: PHOTO_INFO_COUNT }, createPhotoInfo);
