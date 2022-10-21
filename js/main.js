f12e5e5ec3c5c080a9667c4f28600302cc1328aa
Vladimir <avre18c@yandex.ru>
2022-10-21 09:56:09 +0700

    fixed

--- js/main.js
+++ js/main.js
@@ -1,29 +1,34 @@
 const PHOTO_INFO_COUNT = 25;
+const GENER_OBJ_MIN = 1;
+const GENER_OBJ_MAX = 25;
+const LIKE_MIN = 15;
+const LIKE_MAX = 200;
+const COMMENTS_MIN = 0;
+const COMMENTS_MAX = 200;
+
 
 const getRandomPositiveInteger = (a, b) => {
   if (a < 0 || b < 0) {
     return NaN;
   }
+
   const lower = Math.ceil(Math.min(a, b));
   const upper = Math.floor(Math.max(a, b));
-  const result = Math.random() * (upper - lower + 1) + lower;
-  return Math.floor(result);
+  return Math.floor(Math.random() * (upper - lower + 1) + lower);
 };
 
-function checkLengthString(string, maxLength) {
-  const strLength = string.length;
-  return strLength <= maxLength;
-}
+const checkLengthString = (string, maxLength) => string.length <= maxLength;
 
 checkLengthString(2, 5);
 
 const createPhotoInfo = () => ({
-  id: getRandomPositiveInteger(1, 25),
-  url: `photos/${getRandomPositiveInteger(1, 25)}.jpg`,
+  id: getRandomPositiveInteger(GENER_OBJ_MIN, GENER_OBJ_MAX),
+  url: `photos/${getRandomPositiveInteger(GENER_OBJ_MIN, GENER_OBJ_MAX)}.jpg`,
   description: 'Лучшее фото в мире',
-  likes: getRandomPositiveInteger(15, 200),
-  comments: getRandomPositiveInteger(0, 200),
+  likes: getRandomPositiveInteger(LIKE_MIN, LIKE_MAX),
+  comments: getRandomPositiveInteger(COMMENTS_MIN, COMMENTS_MAX),
 });
 
-Array.from({ length: PHOTO_INFO_COUNT }, createPhotoInfo);
+const array = Array.from({ length: PHOTO_INFO_COUNT }, createPhotoInfo);
 
+console.log(array);
